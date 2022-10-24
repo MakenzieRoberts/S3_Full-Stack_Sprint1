b771782f10e8f1c5600a45d213765714dcbcd785
ellis0n <ellisonglenmay@gmail.com>
2022-10-24 15:50:32 -0230

    Co-authored-by: Kara Balsom <Kara.Balsom@keyin.com> Co-authored-by: David Turner <DeToxFox@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # S3_Full-Stack_Sprint1
+
 - Makenzie Roberts
 - Kara Balsom
 - David Turner
 - Glen May
+
+Project Documentation:
+
+LOG FILE READING
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var http = require("http");
 var path = require("path");
 var bodyParser = require("body-parser");
+let { newToken } = require("./token.js");
+global.DEBUG = true;
 
 var app = express();
 var server = http.createServer(app);
@@ -10,27 +12,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "./public")));
 
 app.get("/", function (req, res) {
-	res.sendFile(path.join(__dirname, "./public/form.html"));
+  res.sendFile(path.join(__dirname, "./public/form.html"));
 });
 
 // This function retrieves the username and password from the form
-const getUsername = (request, response) => {
-	const username = request.query.username;
-	const password = request.query.password;
-	console.log("(server.js) - Username Retrieved: " + username);
-	console.log("(server.js) - Password Retrieved: " + password);
-
-	// !KENZI: This next line actually does display the new token on the page, but there's
-	// some strangeness going on in tokens.js For me, the token is displayed on the page,
-	// but the token is not actually saved to the tokens.json file... instead, it writes
-	// "utf-8" to the file, lol.
-
-	// response.status(200).send(`Results: ${JSON.stringify(newToken(username))}`);
+const getUser = (request, response) => {
+  const username = request.query.username;
+  const token = newToken(username);
+  console.log("(server.js) - Username Retrieved: " + username);
+  response.status(200).send(token);
 };
 
-// !KENZI: We can change /view/ to whatever word we want, just keeping it here for now.
-app.get("/view/", getUsername);
+app.get("/view", getUser);
 
 server.listen(3000, function () {
-	console.log("server is listening on port: 3000");
+  console.log("server is listening on port: 3000");
 });
